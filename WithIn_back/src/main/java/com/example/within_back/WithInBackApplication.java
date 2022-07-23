d056a1fc4f89ea64935381d2c6dc93243f718239
SangwonYoon <38793142+SangwonYoon@users.noreply.github.com>
2022-07-23 10:53:55 +0900

    Initial commit

---
+++ WithIn_back/src/main/java/com/example/within_back/WithInBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.within_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithInBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithInBackApplication.class, args);
+	}
+
+}
---
+++ WithIn_back/src/test/java/com/example/within_back/WithInBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.within_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithInBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
