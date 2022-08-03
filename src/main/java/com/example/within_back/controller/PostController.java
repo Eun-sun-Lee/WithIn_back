3c0da41b4520026e871f2bd7c5867ef3ebc28851
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-08-03 22:20:25 +0900

    [feat] add PostController api

--- src/main/java/com/example/within_back/controller/PostController.java
+++ src/main/java/com/example/within_back/controller/PostController.java
@@ -6,26 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/post")
 public class PostController {
-}
-
-@Controller
-@RequestMapping("/post/*")
-public class BoardController {
     @Inject
     BoardService boardService;
     UserService userService;
 
     @RequestMapping(value="/${userId}/boards", method=RequestMethod.GET)
     public void getMyBoard(Model model) throws Exception {
+
+    } //내 게시판 조회
+
+    @RequestMapping(value="/boards/${category}", method=RequestMethod.GET)
+    public void getWrite(Model model) throws Exception {
         List<BoardV0> boardList=null;
         boardList=userService.list();
         model.addAttribute("boardList", boardList);
-    }  //내 게시판 조회
-
-    @RequestMapping(value="/boards/${category}", method=RequestMethod.GET)
-    public void getWrite() throws Exception{
-
     } //게시판 게시물 조회 (GET 방식)
+
 
     @RequestMapping(value="/boards/${category}", method=RequestMethod.POST)
     public String postWrite(BoardV0 vo) throws Exception{
@@ -34,10 +30,12 @@ public class BoardController {
     } //게시물 작성 (POST 방식)
 
     @RequestMapping(value="/boards/${category}/${postId}", method=RequestMethod.GET)
-    public void getWrite
+    public void getOneWrite() throws Exception{
 
-
-
+    } //게시글 조회
 }
+
+
+
 
 
