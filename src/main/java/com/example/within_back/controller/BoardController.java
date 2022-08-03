6d320c332b32ebd26cb87e9dec6d4d91b743d076
SangwonYoon <38793142+SangwonYoon@users.noreply.github.com>
2022-08-03 21:43:13 +0900

    base code

--- src/main/java/com/example/within_back/controller/BoardController.java
+++
@@ -1,4 +0,0 @@
-package com.example.within_back.controller;
-
-public class BoardController {
-}
--- src/main/java/com/example/within_back/controller/UserController.java
+++ src/main/java/com/example/within_back/controller/UserController.java
@@ -1,4 +1,9 @@
 package com.example.within_back.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 }
