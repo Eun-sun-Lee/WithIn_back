4704f667aa69cd8f70ad9ec4aab23afca7526226
SuperH0ng <ghdtmdgus100@gmail.com>
2022-08-07 03:26:16 +0900

    imsi

--- src/main/java/com/example/within_back/dto/CommentReqDto.java
+++ src/main/java/com/example/within_back/dto/CommentReqDto.java
@@ -4,10 +4,6 @@ import com.example.within_back.entity.Comment;
 import com.example.within_back.entity.Post;
 import com.example.within_back.entity.User;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import java.time.LocalDateTime;
-
 public class CommentReqDto {
     private User author;
     private String content;
