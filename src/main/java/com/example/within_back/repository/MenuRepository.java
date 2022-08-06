d0494b64ba7fbc01ca662f7b5c12d2df091e518a
SangwonYoon <38793142+SangwonYoon@users.noreply.github.com>
2022-08-06 17:08:01 +0900

    feat: 식단표 조회 API

--- src/main/java/com/example/within_back/repository/MenuRepository.java
+++ src/main/java/com/example/within_back/repository/MenuRepository.java
@@ -3,5 +3,10 @@ package com.example.within_back.repository;
 import com.example.within_back.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    public ArrayList<Menu> findByBldAndDate(String bld, LocalDate date);
 }
